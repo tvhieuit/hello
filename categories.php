017525e9c1fcf8896dca7ee2baaa35281393be0b
Văn Hiếu <tvhieuit@gmail.com>
2018-11-07 15:41:24 +0700

    Create categories.php

---
+++ categories.php
@@ -0,0 +1,15 @@
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $curlSession = curl_init();
+        curl_setopt($curlSession, CURLOPT_URL, 'http://mrdroidstudiosfootball.xyz/data/scripts/get_categories.php');
+        curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+        $jsonData = curl_exec($curlSession);
+        curl_close($curlSession);
+        echo $jsonData;
+    } else {
+        echo '[]';
+    }
+
+?>

f628dff00ded399b0e6ff7d597a5ec5ee8ae8add
Tran Van Hieu <hieutv@evolableasia.vn>
2018-11-08 16:05:19 +0700

    move to config

--- categories.php
+++ categories.php
@@ -1,8 +1,9 @@
 
 <?php
+    $configs = include('conf.php');
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $curlSession = curl_init();
-        curl_setopt($curlSession, CURLOPT_URL, 'http://mrdroidstudiosfootball.xyz/data/scripts/get_categories.php');
+        curl_setopt($curlSession, CURLOPT_URL, $configs["host"].'/scripts/get_categories.php');
         curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
         curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
         $jsonData = curl_exec($curlSession);
